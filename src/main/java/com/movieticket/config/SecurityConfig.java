6f4c2653f5bd2bc822e16d72bbc5b1a788ccedd8
raviva07 <ravivarma7939@gmail.com>
2026-04-23 22:26:16 +0530

    cors config updated

--- src/main/java/com/movieticket/config/SecurityConfig.java
+++ src/main/java/com/movieticket/config/SecurityConfig.java
@@ -157,13 +157,31 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // frontend origin
-        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+
+   
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://movieticketsbookingfrontend.netlify.app/" 
+        ));
+
+       
+        config.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+
+       
         config.setAllowedHeaders(List.of("*"));
+
+       
+        config.setExposedHeaders(List.of("Authorization"));
+
+        
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
+
         return source;
     }
+
 }
