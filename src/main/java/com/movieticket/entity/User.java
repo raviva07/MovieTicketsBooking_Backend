b6151d3c5e8b4c7dfa54777eaed476c2794b0cf6
raviva07 <ravivarma7939@gmail.com>
2026-04-24 11:27:49 +0530

    updated enity

--- src/main/java/com/movieticket/entity/User.java
+++ src/main/java/com/movieticket/entity/User.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.io.Serializable;
 
-@Document(collection = "users")
+@Document(collection = "movie_users")
 @Getter
 @Setter
 @NoArgsConstructor
